8e1611c386b0df3ce3ec744273908e783cd7f571
azzahra-divania <divvania97@gmail.com>
2022-11-05 10:36:28 +0700

    Modify DB Context

--- Fruitivy/FruitivyDBContext.cs
+++ Fruitivy/FruitivyDBContext.cs
@@ -10,6 +10,7 @@ namespace Fruitivy
 
         }
         //create table di bawah sini (mendaftarkan table dari kelas yg sudah dibuat):
+        //dbcontext
         public DbSet<Pembeli> pembeli { get; set; }
         public DbSet<Penjual> penjual { get; set; }
         public DbSet<Produk> produk { get; set; }
